a919b465126f82be02b30f5c6af3d414702b08b2
saad <saadannadev@gmail.com>
2025-01-08 21:16:51 +0100

    Finish the best seller section and modified some files in the project

--- src/Pages/BestSeller.jsx
+++ src/Pages/BestSeller.jsx
@@ -1,27 +1,69 @@
+import { useState } from "react";
 import PorsheImage from "./PorsheImage.png";
-import MercedesImage from "./MercedesImage.png";
+import MercdesImage from "./MercedesImage.png";
 import FerrariImage from "./FerrariImage.png";
-export default function BestSeller() {
-  return (
-    <>
-      <section className="flex felx-wrap gap-2 mt-32 items-center mx-auto">
-      <div className="flex flex-col w-[35%] gap-2 text-start p-4 bg-[#1d1d2281] backdrop-blur-md shadow-lg rounded-xl duration-300 ease-out hover:bg-[#0c0c0e81]">
-          <h1 className="font-text  text-white font-semibold text-2xl">
-            Porshe 911
-          </h1>
-            <img src={MercedesImage} className="w-[95%] m-auto" />
-          <div className="flex justify-between  items-center">
-          
+const cars = [
+  {
+    id: 1,
+    name: " Porshe 911 GT3",
+    MoreDetails: "View More",
+    description:
+      " Track-bred performance with iconic design. Pure driving thrill.",
+    image: PorsheImage,
+  },
+  {
+    id: 2,
+    name: "Mercedes GLE 300d",
+    MoreDetails: "View More",
+    description: "Luxury, power, and advanced technology in every drive",
+    image: MercdesImage,
+  },
+  {
+    id: 3,
+    name: "Ferrari SF90 ",
+    MoreDetails: "View More",
+    description: "Hybrid power, stunning design, and unmatched performance.",
+    image: FerrariImage,
+  },
+];
 
-            <span className="font-text text-neutral-300 text-base font-normal">
-              1.120.00$
-            </span>
-            <span className="font-text text-neutral-300 text-base font-normal">
-              Shop
-            </span>
+export default function SimpleBestSellerSection() {
+  const [hoveredId, setHoveredId] = useState(null);
+
+  return (
+    <div className="p-4">
+      <div className="flex flex-wrap justify-center items-center gap-4 mt-32">
+        {cars.map((car) => (
+          <div
+            key={car.id}
+            className="relative cursor-pointer w-[300px] h-[400px] bg-[#1d1d224f] backdrop-blur-md shadow-lg rounded-xl p-4"
+            onMouseEnter={() => setHoveredId(car.id)}
+            onMouseLeave={() => setHoveredId(null)}
+          >
+            <img
+              src={car.image}
+              alt={car.name}
+              className="w-full h-full object-contain"
+            />
+            {hoveredId === car.id && (
+              <div className="absolute inset-0 flex gap-2 flex-col text-start bg-black backdrop-blur-sm bg-opacity-20 text-white p-4">
+                <h3 className="font-text text-white font-semibold text-2xl">
+                  {car.name}
+                </h3>
+                <p className="font-text text-neutral-300 text-lg font-normal">
+                  {car.description}
+                </p>
+                <a
+                  href="#"
+                  className="font-text text-lg font-normal text-neutral-500 duration-100 hover:text-neutral-300"
+                >
+                  {car.MoreDetails}
+                </a>
+              </div>
+            )}
           </div>
-        </div>
-      </section>
-    </>
+        ))}
+      </div>
+    </div>
   );
 }
--- src/Pages/Landingpage.jsx
+++ src/Pages/Landingpage.jsx
@@ -101,7 +101,7 @@ export default function Landingpage() {
           className="w-32 h-fit box-border"
         />
       </section>
-      <div className="flex flex-col mt-40 mx-auto justify-center gap-3">
+      <div className="flex flex-col mt-52 mx-auto justify-center gap-3">
         <h1
           data-aos="fade-up"
           data-aos-duration="900"
@@ -117,126 +117,7 @@ export default function Landingpage() {
           Find out about the best-selling cars in Grew
         </p>
       </div>
-      <section
-        data-aos="fade-up"
-        data-aos-duration="900"
-        className="flex flex-wrap mx-auto lg:gap-14 mt-32 justify-center items-center"
-      >
-        <div
-          data-aos="fade-up"
-          data-aos-duration="900"
-          className="flex flex-col text-start w-80 h-80 gap-2  bg-transparent"
-        >
-          <svg
-            className="w-6 h-6 text-white"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            fill="currentColor"
-            viewBox="0 0 24 24"
-          >
-            <path d="M7.833 2c-.507 0-.98.216-1.318.576A1.92 1.92 0 0 0 6 3.89V21a1 1 0 0 0 1.625.78L12 18.28l4.375 3.5A1 1 0 0 0 18 21V3.889c0-.481-.178-.954-.515-1.313A1.808 1.808 0 0 0 16.167 2H7.833Z" />
-          </svg>
 
-          <h1
-            data-aos="fade-up"
-            data-aos-duration="900"
-            className="font-text font-semibold text-xl text-white "
-          >
-            Writing Notes
-          </h1>
-          <p
-            data-aos="fade-up"
-            data-aos-duration="900"
-            className="font-text font-normal text-lg md:text-xl  text-neutral-400"
-          >
-            You can write your daily notes smoothly and simply to ensure that
-            you save your time.{" "}
-          </p>
-        </div>
-        <div
-          data-aos="fade-up"
-          data-aos-duration="900"
-          className="flex flex-col text-start w-80 h-80 gap-2  bg-transparent p-2"
-        >
-          <svg
-            className="w-6 h-6 text-white"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            fill="currentColor"
-            viewBox="0 0 24 24"
-          >
-            <path
-              fillRule="evenodd"
-              d="M12.512 8.72a2.46 2.46 0 0 1 3.479 0 2.461 2.461 0 0 1 0 3.479l-.004.005-1.094 1.08a.998.998 0 0 0-.194-.272l-3-3a1 1 0 0 0-.272-.193l1.085-1.1Zm-2.415 2.445L7.28 14.017a1 1 0 0 0-.289.702v2a1 1 0 0 0 1 1h2a1 1 0 0 0 .703-.288l2.851-2.816a.995.995 0 0 1-.26-.189l-3-3a.998.998 0 0 1-.19-.26Z"
-              clipRule="evenodd"
-            />
-            <path
-              fillRule="evenodd"
-              d="M7 3a1 1 0 0 1 1 1v1h3V4a1 1 0 1 1 2 0v1h3V4a1 1 0 1 1 2 0v1h1a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V7a2 2 0 0 1 2-2h1V4a1 1 0 0 1 1-1Zm10.67 8H19v8H5v-8h3.855l.53-.537a1 1 0 0 1 .87-.285c.097.015.233.13.277.087.045-.043-.073-.18-.09-.276a1 1 0 0 1 .274-.873l1.09-1.104a3.46 3.46 0 0 1 4.892 0l.001.002A3.461 3.461 0 0 1 17.67 11Z"
-              clipRule="evenodd"
-            />
-          </svg>
-
-          <h1
-            data-aos="fade-up"
-            data-aos-duration="900"
-            className="font-text font-semibold text-xl text-white "
-          >
-            Writing assignments
-          </h1>
-          <p
-            data-aos="fade-up"
-            data-aos-duration="900"
-            className="font-text font-normal text-lg md:text-xl  text-neutral-400"
-          >
-            Write down your monthly or even daily assignments and keep your
-            focus on them.
-          </p>
-        </div>
-        <div
-          data-aos="fade-up"
-          data-aos-duration="900"
-          className="flex flex-col text-start w-80 h-80 gap-2  bg-transparent p-2"
-        >
-          <svg
-            className="w-6 h-6 text-white"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            fill="currentColor"
-            viewBox="0 0 24 24"
-          >
-            <path d="M11.209 3.816a1 1 0 0 0-1.966.368l.325 1.74a5.338 5.338 0 0 0-2.8 5.762l.276 1.473.055.296c.258 1.374-.228 2.262-.63 2.998-.285.52-.527.964-.437 1.449.11.586.22 1.173.75 1.074l12.7-2.377c.528-.1.418-.685.308-1.27-.103-.564-.636-1.123-1.195-1.711-.606-.636-1.243-1.306-1.404-2.051-.233-1.085-.275-1.387-.303-1.587-.009-.063-.016-.117-.028-.182a5.338 5.338 0 0 0-5.353-4.39l-.298-1.592Z" />
-            <path
-              fillRule="evenodd"
-              d="M6.539 4.278a1 1 0 0 1 .07 1.412c-1.115 1.23-1.705 2.605-1.83 4.26a1 1 0 0 1-1.995-.15c.16-2.099.929-3.893 2.342-5.453a1 1 0 0 1 1.413-.069Z"
-              clipRule="evenodd"
-            />
-            <path d="M8.95 19.7c.7.8 1.7 1.3 2.8 1.3 1.6 0 2.9-1.1 3.3-2.5l-6.1 1.2Z" />
-          </svg>
-
-          <h1
-            data-aos="fade-up"
-            data-aos-duration="900"
-            className="font-text font-semibold text-xl text-white "
-          >
-            Pomodoro Time
-          </h1>
-          <p
-            data-aos="fade-up"
-            data-aos-duration="900"
-            className="font-text font-normal text-lg md:text-xl  text-neutral-400"
-          >
-            Using this technique can be very helpful in distributing your work
-            time.{" "}
-          </p>
-        </div>
-      </section>
       <BestSeller />
     </div>
   );
